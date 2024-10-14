0c5b5244bc9e20d59a5164b1fbe4afc61d5d0ec0
Paolo Tagliolato <paolo.tagliolato@gmail.com>
2024-10-14 13:51:55 +0200

    Add excel file to prepare data-raw

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,9 +3,9 @@
 # execute once when needed
 require(readxl)
 
-sites<-readxl::read_xlsx("static_data/sites_list.xlsx")
-EVs<-readxl::read_xlsx("static_data/sites_list.xlsx", "EV")
-datasets<-readxl::read_xlsx("static_data/sites_list.xlsx", "datasets")
+sites<-readxl::read_xlsx("data-raw/sites_list.xlsx")
+EVs<-readxl::read_xlsx("data-raw/sites_list.xlsx", "EV")
+datasets<-readxl::read_xlsx("data-raw/sites_list.xlsx", "datasets")
 
 usethis::use_data(sites, EVs, datasets, overwrite = TRUE, internal = TRUE)
 
