c685a73b4b1a3e48e3641a1f98f341b26f70dff4
scostafreda <scaumedes@gmail.com>
2024-10-14 16:26:58 +0200

    Data preparation

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,7 +5,7 @@ require(readxl)
 
 sites<-readxl::read_xlsx("D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/data-raw/sites_list.xlsx")
 EVs<-readxl::read_xlsx("D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/data-raw/sites_list.xlsx", "EV")
-datasets<-readxl::read_xlsx("D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/ata-raw/sites_list.xlsx", "datasets")
+datasets<-readxl::read_xlsx("D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/data-raw/sites_list.xlsx", "datasets")
 
 usethis::use_data(sites, EVs, datasets, overwrite = TRUE, internal = TRUE)
 
