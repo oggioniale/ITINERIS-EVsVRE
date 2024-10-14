a32b7f204bc1e0850911876dd7ee2b398dc3e3a2
scostafreda <scaumedes@gmail.com>
2024-10-14 17:09:52 +0200

    Collelongo e test prime funzioni

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,9 +3,9 @@
 # execute once when needed
 require(readxl)
 
-sites<-readxl::read_xlsx("D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/data-raw/sites_list.xlsx")
-EVs<-readxl::read_xlsx("D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/data-raw/sites_list.xlsx", "EV")
-datasets<-readxl::read_xlsx("D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/data-raw/sites_list.xlsx", "datasets")
+sites<-readxl::read_xlsx("D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/data-raw/sites_list.xlsx") # SERGI
+EVs<-readxl::read_xlsx("D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/data-raw/sites_list.xlsx", "EV") # SERGI
+datasets<-readxl::read_xlsx("D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/data-raw/sites_list.xlsx", "datasets") # SERGI
 
 usethis::use_data(sites, EVs, datasets, overwrite = TRUE, internal = TRUE)
 
