f0ccd1a08721f5ca2367720a71b40f39ff2f82e1
scostafreda <scaumedes@gmail.com>
2024-10-15 10:05:15 +0200

    Reverting DATASET

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,9 +3,9 @@
 # execute once when needed
 require(readxl)
 
-sites<-readxl::read_xlsx("D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/data-raw/sites_list.xlsx") # SERGI
-EVs<-readxl::read_xlsx("D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/data-raw/sites_list.xlsx", "EV") # SERGI
-datasets<-readxl::read_xlsx("D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/data-raw/sites_list.xlsx", "datasets") # SERGI
+sites<-readxl::read_xlsx("data-raw/sites_list.xlsx") # SERGI  D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/
+EVs<-readxl::read_xlsx("data-raw/sites_list.xlsx", "EV") # SERGI
+datasets<-readxl::read_xlsx("data-raw/sites_list.xlsx", "datasets") # SERGI
 
 usethis::use_data(sites, EVs, datasets, overwrite = TRUE, internal = TRUE)
 
