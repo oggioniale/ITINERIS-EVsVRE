b8b1727c8ab5a49189c7d2c8d3d56913be7f18d9
scostafreda <scaumedes@gmail.com>
2024-12-19 16:27:14 +0100

    Update file excel

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,8 +4,8 @@
 require(readxl)
 
 sites<-readxl::read_xlsx("data-raw/sites_list.xlsx") # SERGI  D:/Projects/ITINERIS/Server/ITINERIS-EVsVRE/
-EVs<-readxl::read_xlsx("data-raw/sites_list.xlsx", "EV") # SERGI
-datasets<-readxl::read_xlsx("data-raw/sites_list.xlsx", "datasets") # SERGI
+EVs<-readxl::read_xlsx("data-raw/sites_list.xlsx", "EV")
+datasets<-readxl::read_xlsx("data-raw/sites_list.xlsx", "datasets")
 
 usethis::use_data(sites, EVs, datasets, overwrite = TRUE, internal = TRUE)
 
